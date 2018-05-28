0928f2c64769c3a77b762ab903831c75ae00b35e
XingYunLi <contact@idrmfly.com>
2018-05-28 09:16:25 +0800

    first commit

---
+++ page.go
@@ -0,0 +1,130 @@
+package page
+
+import (
+	"fmt"
+
+	"github.com/go-xorm/xorm"
+)
+
+// Sort sort
+type Sort struct {
+	Sortby string
+	Order  string
+}
+
+// Pageable pageable
+type Pageable struct {
+	PageIndex int
+	PageSize  int
+	offset    int
+	Sort      Sort
+}
+
+//SetDefault default page
+func (p *Pageable) SetDefault() {
+	if p.PageIndex == 0 {
+		p.PageIndex = 1
+	}
+	if p.PageSize == 0 {
+		p.PageSize = 10
+
+	}
+}
+
+//Check verify page inde & page size.
+func (p *Pageable) Check() error {
+	if p.PageIndex == 0 {
+		return fmt.Errorf("Pageable must begin with 1, actual is %d", p.PageIndex)
+	}
+	if p.PageSize == 0 {
+		return fmt.Errorf("Page size can not be zero")
+	}
+	return nil
+}
+
+//Offset db start
+func (p *Pageable) Offset() int {
+	if p.offset == 0 {
+		p.offset = (p.PageIndex - 1) * p.PageSize
+	}
+	return p.offset
+}
+
+//Page page
+type Page struct {
+	PageIndex int         `json:"page_index,omitempty"`
+	PageSize  int         `json:"page_size,omitempty"`
+	Pages     int64       `json:"pages,omitempty"`
+	Total     int64       `json:"total,omitempty"`
+	Data      interface{} `json:"data,omitempty"`
+}
+
+//NewPage page
+func NewPage(data interface{}) *Page {
+	return &Page{}
+}
+
+//PageBuilder page builder
+type PageBuilder struct {
+	pageable *Pageable
+	page     Page
+	session  *xorm.Session
+}
+
+//Page init page index & size
+func (p *PageBuilder) Page(pa *Pageable) *PageBuilder {
+	p.pageable = pa
+	return p
+}
+
+//Total set total elments & total pages
+func (p *PageBuilder) Total(total int64) *PageBuilder {
+	p.page.Total = total
+	return p
+}
+
+//Data set page data
+func (p *PageBuilder) Data(data interface{}) *PageBuilder {
+	p.page.Data = data
+	return p
+}
+
+//Session set xorm session
+func (p *PageBuilder) Session(session *xorm.Session) *PageBuilder {
+	p.session = session
+	return p
+}
+
+//Build return page struct
+func (p *PageBuilder) Build() (*Page, error) {
+	err := p.pageable.Check()
+	if err != nil {
+		return nil, fmt.Errorf("Pageable error,err=%s", err)
+	}
+	p.page.PageIndex = p.pageable.PageIndex
+	p.page.PageSize = p.pageable.PageSize
+
+	if p.page.Total%int64(p.page.PageSize) == 0 {
+		p.page.Pages = p.page.Total / int64(p.page.PageSize)
+	} else {
+		p.page.Pages = p.page.Total/int64(p.page.PageSize) + 1
+	}
+
+	p.session.Limit(p.pageable.PageSize, p.pageable.Offset()).Find(p.page.Data)
+	defer p.session.Close()
+	return &(p.page), nil
+}
+
+//NewPageBuilder new page builder
+func NewPageBuilder() *PageBuilder {
+	return &PageBuilder{}
+}
+
+// PageableMapper pageable base mapper
+type PageableMapper struct {
+}
+
+//PageBuilder get mapper pagebuilder
+func (p *PageableMapper) PageBuilder() *PageBuilder {
+	return NewPageBuilder()
+}
